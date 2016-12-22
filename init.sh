734642d394554f99559741be34bd6720307fe333
Miron Ogrodowicz <miron.ogrodowicz@gmail.com>
2016-12-22 13:39:13 +0100

    init script

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TMP_SETUP_DIR="tmp/docker-setup"
+CURRENT_PWD=$PWD
+
+if [ ! -d $TMP_SETUP_DIR ]
+  then
+    mkdir -p $TMP_SETUP_DIR
+fi
+
+cd $TMP_SETUP_DIR
+
+git clone --depth 1 git@github.com:micron/docker-setup.git .
+
+rsync -rv --ignore-existing . $CURRENT_PWD/
