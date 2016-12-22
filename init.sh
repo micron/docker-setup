e4bee1c5aa2b0ca58dddb889c41016e67bc2a161
Miron Ogrodowicz <miron.ogrodowicz@gmail.com>
2016-12-22 14:04:31 +0100

    modified cleanup task

--- init.sh
+++ init.sh
@@ -2,14 +2,22 @@
 
 TMP_SETUP_DIR="tmp/docker-setup"
 CURRENT_PWD=$PWD
+CLEANUP=false
 
 if [ ! -d $TMP_SETUP_DIR ]
   then
     mkdir -p $TMP_SETUP_DIR
+    CLEANUP=true
 fi
 
 cd $TMP_SETUP_DIR
 
 git clone --depth 1 git@github.com:micron/docker-setup.git .
 
-rsync -rv --ignore-existing --exclude=./init.sh . $CURRENT_PWD/
+rsync -rv --dry-run --ignore-existing --exclude='./init.sh' --exclude='./.git' . $CURRENT_PWD/
+
+if [ "$CLEANUP" = true ]
+  then
+    cd $CURRENT_PWD
+    rm -rf $TMP_SETUP_DIR
+fi
